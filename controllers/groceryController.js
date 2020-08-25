95af151fc72b7cef37dac33f4f0dfceb67dfed16
Quinton Fults <quinton.fults.work@gmail.com>
2020-08-25 14:30:46 -0700

    code

---
+++ controllers/groceryController.js
@@ -0,0 +1,24 @@
+const router = require('express').Router()
+const grocery = require('../models/grocery.js')
+
+// Get all groceries
+router.get('/groceries', (req, res) => {
+
+})
+
+// Create one grocery
+router.post('/groceries', (req, res) => {
+
+})
+
+// Update one grocery
+router.put('/groceries/:id', (req, res) => {
+
+})
+
+// Delete one grocery
+router.delete('/groceries/:id', (req, res) => {
+
+})
+
+module.exports = router
