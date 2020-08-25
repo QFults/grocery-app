36be5becbf4772cea52a7dd39ee7a117443e2782
Quinton Fults <quinton.fults.work@gmail.com>
2020-08-25 14:03:49 -0700

    code

---
+++ db/seed.sql
@@ -0,0 +1,14 @@
+USE grocery_db;
+
+INSERT INTO groceries (name, quantity, cost)
+VALUES ('Milk', 1, 3.00),
+  ('Carrots', 5, 1.50),
+  ('Cereal', 1, 3),
+  ('Oranges', 5, 3.25),
+  ('Bread', 2, 4.50),
+  ('Candy', 1, 1.25),
+  ('Oreos', 1, 2.50),
+  ('Cookies', 1, 2.50),
+  ('Eggs', 1, 4),
+  ('Yogourt', 1, 3.5);
+  
