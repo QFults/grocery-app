20c99a2c2b3a69e9088890253b80aaef8375b3bc
Quinton Fults <quinton.fults.work@gmail.com>
2020-08-26 10:36:16 -0700

    code

--- db/seed.sql
+++ db/seed.sql
@@ -1,14 +1,13 @@
 USE grocery_db;
 
-INSERT INTO groceries (name, quantity, cost)
-VALUES ('Milk', 1, 3.00),
-  ('Carrots', 5, 1.50),
-  ('Cereal', 1, 3),
-  ('Oranges', 5, 3.25),
-  ('Bread', 2, 4.50),
-  ('Candy', 1, 1.25),
-  ('Oreos', 1, 2.50),
-  ('Cookies', 1, 2.50),
-  ('Eggs', 1, 4),
-  ('Yogourt', 1, 3.5);
-  
+INSERT INTO groceries (name, quantity, cost, purchased)
+VALUES ('Milk', 1, 3.00, false),
+  ('Carrots', 5, 1.50, false),
+  ('Cereal', 1, 3, true),
+  ('Oranges', 5, 3.25, true),
+  ('Bread', 2, 4.50, false),
+  ('Candy', 1, 1.25, false),
+  ('Oreos', 1, 2.50, true),
+  ('Cookies', 1, 2.50, true),
+  ('Eggs', 1, 4, false),
+  ('Yogurt', 1, 3.5, false);
